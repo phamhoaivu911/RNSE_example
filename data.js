defea015efdd8a9a06f978fad7ce80e242198d8c
Vu PHAM <vu.pham@employmenthero.com>
2020-03-26 20:24:49 +0700

    init

---
+++ data.js
@@ -0,0 +1,141 @@
+export default [
+  {
+    name: 'Masen Compton',
+    email: 'masen.compton@gmail.com',
+    position: 'Human Resources',
+    photo:
+      'https://images.generated.photos/T3QjWL8F7RltyskLJWg5enb5OJwKAJDpej10XTM7LQw/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zLzAy/NzgzODEuanBn.jpg',
+  },
+  {
+    name: 'Emma Dumont',
+    email: 'emma.dumont@gmail.com',
+    position: 'Office Assistant',
+    photo:
+      'https://images-na.ssl-images-amazon.com/images/M/MV5BYjhlYjA0YTUtNjlmMS00NzQ4LWJlMjYtZjYzMTQ1MzcyMmY3XkEyXkFqcGdeQXVyMTkyOTc4MTM@._V1_UY256_CR19,0,172,256_AL_.jpg',
+  },
+  {
+    name: 'Sasha Pieterse',
+    email: 'sasha.pieterse@gmail.com',
+    position: 'Executive Assistant',
+    photo:
+      'https://images-na.ssl-images-amazon.com/images/M/MV5BMjM2NDIwNzUyNV5BMl5BanBnXkFtZTgwMDE4ODI1MjE@._V1_UX172_CR0,0,172,256_AL_.jpg',
+  },
+  {
+    name: 'Merritt Patterson',
+    email: 'merritt.patterson@gmail.com',
+    position: 'Sales',
+    photo:
+      'https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5NzQ0OTQ5NV5BMl5BanBnXkFtZTgwNzgxOTMzNjE@._V1_UX172_CR0,0,172,256_AL_.jpg',
+  },
+  {
+    name: 'Sameer Gurav',
+    email: 'sameer.gurav@gmail.com',
+    position: 'Attorney',
+    photo:
+      'http://pbs.twimg.com/profile_images/1064455649/25694_111934538820096_100000106519899_276355_8033866_n.jpg',
+  },
+  {
+    name: 'Korbyn Colon',
+    email: 'korbyn.colon@gmail.com',
+    position: 'Marketing',
+    photo:
+      'https://images.unsplash.com/photo-1498190718497-403407ff7eb2?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=a51c09ba28b70eace6e53adc15ae90ba',
+  },
+  {
+    name: 'Ryu Duke',
+    email: 'ryu.duke@gmail.com',
+    position: 'Marketing',
+    photo:
+      'https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=ddcb7ec744fc63472f2d9e19362aa387',
+  },
+  {
+    name: 'Gabriella Horton',
+    email: 'gabriella.horton@gmail.com',
+    position: 'Product Designer',
+    photo:
+      'https://images.unsplash.com/photo-1546967191-fdfb13ed6b1e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+  },
+  {
+    name: 'Katheryn Winnick',
+    email: 'katheryn.winnick@gmail.com',
+    position: 'Receptionist',
+    photo:
+      'https://images-na.ssl-images-amazon.com/images/M/MV5BNzY3ZDUxYTItOWI2My00NmQ5LWI5M2ItZmRlNTZiYTgyYmMyXkEyXkFqcGdeQXVyMTE1MzA3MTI@._V1_UX172_CR0,0,172,256_AL_.jpg',
+  },
+  {
+    name: 'Spencer Horton',
+    email: 'spencer.horton@gmail.com',
+    position: 'Accountant',
+    photo:
+      'https://images.unsplash.com/photo-1548532928-b34e3be62fc6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+  },
+  {
+    name: 'Rosalee Melvin',
+    email: 'rosalee.melvin@gmail.com',
+    position: 'Product Designer',
+    photo:
+      'https://images.unsplash.com/photo-1512473199894-0cee3b672650?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+  },
+  {
+    name: 'Lamar Buckner',
+    email: 'lamar.buckner@gmail.com',
+    position: 'Part Time',
+    photo:
+      'https://images.generated.photos/tVZdtsXln-D9HXNrWYSUD3gtQQP-VyMrufgsSyIuWJI/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yy/XzAyMDMwMDMuanBn.jpg',
+  },
+  {
+    name: 'Kelly Reilly',
+    email: 'kelly.reilly@gmail.com',
+    position: 'Attorney',
+    photo:
+      'https://images-na.ssl-images-amazon.com/images/M/MV5BNjgzMzk2MjEzM15BMl5BanBnXkFtZTgwMTkzMDEwMTE@._V1_UX172_CR0,0,172,256_AL_.jpg',
+  },
+  {
+    name: 'Katheryn Evans',
+    email: 'katheryn.evans@gmail.com',
+    position: 'Software Engineer',
+    photo:
+      'https://m.media-amazon.com/images/M/MV5BNTFmMjg2MTYtZjY4Ni00ZTQzLWI2OWMtNTA5NWQ0OGJhZWQ3XkEyXkFqcGdeQXVyMTE1MzA3MTI@._V1_UY256_CR106,0,172,256_AL_.jpg',
+  },
+  {
+    name: 'Marcia Gaudencio',
+    email: 'marcia.gaudencio@gmail.com',
+    position: 'Data Entry',
+    photo:
+      'http://pbs.twimg.com/profile_images/752461099807678464/DUhuoeI8.jpg',
+  },
+  {
+    name: 'Didier Brun',
+    email: 'didier.brun@gmail.com',
+    position: 'Accounting',
+    photo:
+      'http://pbs.twimg.com/profile_images/442752287241293824/mTsB6gg0.jpeg',
+  },
+  {
+    name: 'Amber Heard',
+    email: 'amber.heard@gmail.com',
+    position: 'Accountant',
+    photo:
+      'https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ0MjA1ODU0MV5BMl5BanBnXkFtZTgwNTczNTkwNjE@._V1_UY256_CR8,0,172,256_AL_.jpg',
+  },
+  {
+    name: 'Lucrecia Calder',
+    email: 'lucrecia.calder@gmail.com',
+    position: 'Attorney',
+    photo: 'https://randomuser.me/api/portraits/women/95.jpg',
+  },
+  {
+    name: 'Craig Chaney',
+    email: 'craig.chaney@gmail.com',
+    position: 'Attorney',
+    photo:
+      'https://images.unsplash.com/photo-1542345812-d98b5cd6cf98?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+  },
+  {
+    name: 'Alejandra Stallings',
+    email: 'alejandra.stallings@gmail.com',
+    position: 'Customer Service Representative',
+    photo:
+      'https://images.unsplash.com/photo-1498597531364-5944cd5388f4?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=154bbabc57d958ef985edbd93c1cd051',
+  },
+];
